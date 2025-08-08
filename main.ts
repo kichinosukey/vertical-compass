fd6a7d293464f49956822aa4849e8d33e7998439
kichinosukey <kichinosuke.fukuhara@gmail.com>
2025-08-08 18:09:43 +0900

    更新 main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
+/**
+ * 垂直に持ったmicro:bitで方角を判定する拡張
+ */
+//% weight=100 color=#0fbc11 icon="🧭"
 namespace VerticalCompass {
     /**
      * XとZの地磁気から方角（0:東, 1:北, 2:西, 3:南）を返す
@@ -18,11 +22,15 @@ namespace VerticalCompass {
         return dir
     }
 
+    /**
+     * XとZの地磁気から角度（0〜359度）を返す
+     */
+    //% block
     export function getVAngle(magX: number, magZ: number): number {
-        let angle2 = Math.atan2(magZ, magX) * 180 / Math.PI
-        if (angle2 < 0) {
-            angle2 += 360
+        let angle = Math.atan2(magZ, magX) * 180 / Math.PI
+        if (angle < 0) {
+            angle += 360
         }
-        return Math.round(angle2)
+        return Math.round(angle)
     }
 }
